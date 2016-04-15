1d69d3cbaec5d1e5ed9ec35db4651d444f4da724
Artur Eshenbrener <strate@yandex.ru>
2016-04-15 16:24:16 +0300

    Initial

---
+++ README.markdown
@@ -0,0 +1,12 @@
+# sigh-await-and-merge
+
+[![build status](https://circleci.com/gh/Strate/sigh-await-and-merge.png)](https://circleci.com/gh/Strate/sigh-await-and-merge)
+
+Sigh plugin for...
+
+## Example
+
+`npm install --save-dev sigh-await-and-merge` then add something like this to your `sigh.js`:
+```javascript
+// TODO: example goes here
+```
---
+++ src/test/index.spec.js
@@ -0,0 +1,15 @@
+import _ from 'lodash'
+import Promise from 'bluebird'
+import { Bacon } from 'sigh-core'
+import Event from 'sigh/lib/Event'
+
+import awaitAndMerge from '../'
+
+require('source-map-support').install()
+require('chai').should()
+
+describe('sigh-await-and-merge', () => {
+  xit('TODO: should do something', () => {
+    // TODO:
+  })
+})
